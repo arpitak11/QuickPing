d2a73ef4100f96c0390f899a8690e2b0f514d879
Arpita <arpitakri1183@gmail.com>
2025-06-07 00:18:41 +0530

    middleware and controllers changed

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+export const signup=(req, res) =>{
+    res.send("signupUser")
+    console.log("signupUser");
+}
+
+export const login=(req, res) =>{
+    console.log("loginUser");
+}
+
+export const logout=(req, res) =>{
+    console.log("logoutUser");
+}
+
+
+
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-const express= require("express")
+//imports express
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js"
+
+const app=express(); // start server
+
+dotenv.config();
+const PORT=process.env.PORT || 5000; //either 5000 or PORT 8000
+//test route
+app.get("/", (req, res)=>{
+    //root route 5000
+    res.send("hello world!!");
+})
+
+app.use("/api/auth", authRoutes); // whenever we will go to /api/auth we will go to authRoutes
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); //it makes listening on port 5000
