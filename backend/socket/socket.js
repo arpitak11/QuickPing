efa5cf17cda03360f020c25400b4ec9ab4b6e328
Arpita <arpitakri1183@gmail.com>
2025-08-09 19:56:59 +0530

    real time fix

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://quickping-2.onrender.com/"],
     methods: ["GET", "POST"],
   },
 });
@@ -25,7 +25,7 @@ io.on("connection", (socket) => {
   const userId= socket.handshake.query.userId;
   if(userId != "undefined") userSocketMap[userId]=socket.id;
 
-  io.emit("getOnlineUser", Object.keys(userSocketMap));
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   //soket.on() is used to listen to the events, can be used both on client and server side
   socket.on("disconnect", () => {
